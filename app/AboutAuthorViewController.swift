ec6a98384f374f6537a2b009ef6ad6044a010fb8
Ellen Shimada <ellenshimada@gmail.com>
2019-07-10 18:14:01 -0700

    Image files

---
+++ app/AboutAuthorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by e10a on 7/10/19.
+//  Copyright © 2019 e10a. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
